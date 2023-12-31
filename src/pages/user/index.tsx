a9ab7d60986ef164d2adfe36b15ebb00fc719c5f
97revenge <empregos97@gmail.com>
2023-12-31 19:31:29 -0300

    feat:a Next.js TypeScript component fetching user data from the GitHub API using getServerSideProps. Renders user details if the request is successful; otherwise, displays an error message in Portuguese for incorrect login.

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -3,18 +3,23 @@ import React from "react";
 
 import Link from "next/link";
 
-export default function User({ user }: { user: any }) {
+export default function User({ user, request }: { user: any; request: any }) {
   const { login, url, location } = user;
 
   return (
     <>
-      <div>{login}</div>
-      <div>{url}</div>
-      <div>{location}</div>
-
-      <Link href={{ pathname: "/repos", query: { username: login } }}>
-        Repos
-      </Link>
+      {request == "200" ? (
+        <>
+          <div>{login}</div>
+          <div>{url}</div>
+          <div>{location}</div>
+          <Link href={{ pathname: "/repos", query: { username: login } }}>
+            Repos
+          </Link>
+        </>
+      ) : (
+        <>voce está com o login errado</>
+      )}
     </>
   );
 }
@@ -22,11 +27,13 @@ export default function User({ user }: { user: any }) {
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const instance = query?.username;
   const response = await fetch(`https://api.github.com/users/${instance}`);
+  const newData = response.status;
   const data = await response.json();
 
   return {
     props: {
       user: data,
+      request: newData,
     },
   };
 };
